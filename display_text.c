1fa972bef281fa114804e6833bf5eb3d2647ef8a
IT25102804 <it25102804@my.sliit.lk>
2025-07-15 15:05:27 +0530

    Create display_text.c

---
+++ display_text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello! Welcome to SlIIT\n");
+
+    printf("First line\n" "This is the second line\n");
+
+    printf("  x\n" " x x\n" "x   x\n"" x x\n""x   x\n"" x x\n""x   x\n"" x x\n""  x\n");
+
+    return 0;
+}
